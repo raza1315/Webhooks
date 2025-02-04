f73b82a5881b2635a0aca0a2ab873f3329e31f90
raza1315 <razasfs2003@gmail.com>
2025-02-04 13:07:48 +0530

    index file for express server containing the webhook url to be pinged

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app =express();
+app.use(express.json());
+const dataArr=[];
+app.listen(3000,()=>{
+console.log('server is running on port 3000');
+})
+app.get('/',(req,res)=>{
+res.status(200).json(`Data is : ${dataArr}`)
+})
+app.post('/webhook',(req,res)=>{
+const data=req.body;
+console.log(`someone pinged the webhook and the name is : ${data.name}`);
+dataArr.push(data.name);
+res.status(200).json("Webhook pinged successfully!");
+})
